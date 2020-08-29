7055dd41e0c83f2bd7099bf62dc981c9e6edce9d
noaheakin <noaheakin@gmail.com>
2020-08-29 14:40:35 -0700

    passed all tests

---
+++ lib/song.rb
@@ -0,0 +1,49 @@
+class Song
+
+    attr_reader :name, :artist, :genre
+
+    @@artists = []
+    @@genres = []
+    @@genre_count = {}
+    @@count = 0
+
+    def initialize (name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@artists << artist
+        @@genres << genre
+        @@count += 1
+        
+    end
+
+    def Song.count
+        @@count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    # def genre_hash
+    #     genre_count.each do |key, value|
+    #         if key == genre
+    #             value += 1
+    #         else 
+    #             genre_count[genre] = 1
+    #         end
+    #     end
+    # end
+
+    def Song.genre_count
+        @@genres.each_with_object(Hash.new(0)) {|element, hash| hash[element] += 1} 
+    end
+
+    def Song.artist_count
+        @@artists.each_with_object(Hash.new(0)) {|element, hash| hash[element] += 1}
+    end
+end
